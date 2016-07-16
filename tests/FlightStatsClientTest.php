a579d5d3dfb246b6a004ab8ae638288118c6b8ec
Alan Ruvalcaba <aruval3@gmail.com>
2016-07-16 15:36:16 +0000

    Add test class for FlightStatsClient

---
+++ tests/FlightStatsClientTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace BlackLabel\FlightStats;
+
+use GuzzleHttp\Client;
+
+use BlackLabel\FlightStats\Exception\IncompleteClientConfigException;
+
+class FlightStatsClientTest extends FlightStatsTest
+{
+    public function incompleteConfigProvider()
+    {
+        $appId = $_ENV['APP_ID'];
+        $appKey = $_ENV['APP_KEY'];
+
+        return [
+            [ [] ],
+            [ ['appId' => $appId] ],
+            [ ['appKey' => $appKey] ],
+            [ ['app_key' => $appKey, 'app_id' => $appId] ]
+        ];
+    }
+
+    public function completeConfigProvider()
+    {
+        $appId = $_ENV['APP_ID'];
+        $appKey = $_ENV['APP_KEY'];
+
+        return [
+            [ ['appId' => $appId,'appKey' => $appKey] ],
+            [ ['appId' => $appId,'appKey' => $appKey,'extraKey' => 'EXTRA_KEY'] ],
+        ];
+    }
+
+    /**
+     * @dataProvider incompleteConfigProvider
+     */
+    public function testConstructWithInCompleteConfig(array $config)
+    {
+        $this->setExpectedException(IncompleteClientConfigException::class);
+
+        $httpClient = new Client();
+
+        $client = new FlightStatsClient($httpClient,$this->apiUrl,$config);
+    }
+    
+    /**
+     * @dataProvider completeConfigProvider
+     * @depends testConstructWithInCompleteConfig
+     */
+    public function testConstructWithCompleteConfig(array $config)
+    {
+        $httpClient = new Client();
+        
+        $client = new FlightStatsClient($httpClient,$this->apiUrl,$config);
+    }
+}
