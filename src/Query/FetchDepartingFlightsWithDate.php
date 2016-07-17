8ee9cad40fc940c6a6634b09f35d9a36fed6b335
Alan Ruvalcaba <aruval3@gmail.com>
2016-07-17 18:09:01 +0000

    Refactor classes to extend FetchFlightsWithDate

--- src/Query/FetchDepartingFlightsWithDate.php
+++ src/Query/FetchDepartingFlightsWithDate.php
@@ -6,20 +6,7 @@ namespace BlackLabel\FlightStats\Query;
  * @author Alan Ruvalcaba <aruval3@gmail.com>
  * @copyright 2016 Alan Ruvalcaba
  */
-class FetchDepartingFlightsWithDate extends Query
+class FetchDepartingFlightsWithDate extends FetchFlightsWithDate
 {
-    public function invoke(array $params =[])
-    {
-        $format = 'schedules/rest/v1/json/from/%s/to/%s/departing/%s/%s/%s';
-        
-        $departureAirportCode = $params['departureAirportCode'];
-        $arrivalAirportCode = $params['arrivalAirportCode'];
-        $year = $params['year'];
-        $month = $params['month'];
-        $day = $params['day'];
-
-        $route = sprintf($format,$departureAirportCode,$arrivalAirportCode,$year,$month,$day);
-
-        return $this->client->request($route);
-    }
+    protected $method = 'departing';
 }
